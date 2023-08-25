a0c8b00b403fd847281deb41c9a2311c67b310f5
ethan cohen <55588944+ethancohen123@users.noreply.github.com>
2023-08-25 13:26:54 +0200

    Create roberta_zinc.py
    add roberta encoder+tokenizer

---
+++ imagen_pytorch/roberta_zinc.py
@@ -0,0 +1,53 @@
+import torch
+from transformers import RobertaTokenizerFast, RobertaForMaskedLM, DataCollatorWithPadding
+from typing import List
+
+# config
+DEFAULT_ROBERTA_NAME = 'entropy/roberta_zinc_480m'
+MAX_LENGTH = 128
+
+ROBERTA_CONFIGS = {}
+
+# singleton globals
+def get_tokenizer(name):
+    tokenizer = RobertaTokenizerFast.from_pretrained(name, max_len=MAX_LENGTH)
+    return tokenizer
+
+def get_model(name):
+    model = RobertaForMaskedLM.from_pretrained(name)
+    return model
+
+def get_model_and_tokenizer(name):
+    global ROBERTA_CONFIGS
+
+    if name not in ROBERTA_CONFIGS:
+        ROBERTA_CONFIGS[name] = dict()
+    if "model" not in ROBERTA_CONFIGS[name]:
+        ROBERTA_CONFIGS[name]["model"] = get_model(name)
+    if "tokenizer" not in ROBERTA_CONFIGS[name]:
+        ROBERTA_CONFIGS[name]["tokenizer"] = get_tokenizer(name)
+
+    return ROBERTA_CONFIGS[name]['model'], ROBERTA_CONFIGS[name]['tokenizer']
+
+def roberta_encode_text(
+    texts: List[str],
+    name=DEFAULT_ROBERTA_NAME,
+    return_attn_mask=False
+):
+    model, tokenizer = get_model_and_tokenizer(name)
+    collator = DataCollatorWithPadding(tokenizer, padding=True, return_tensors='pt')
+
+    inputs = collator(tokenizer(texts))
+    model.eval()
+
+    with torch.no_grad():
+        outputs = model(**inputs, output_hidden_states=True)
+        full_embeddings = outputs[1][-1]
+
+    mask = inputs['attention_mask']
+    embeddings = ((full_embeddings * mask.unsqueeze(-1)).sum(1) / mask.sum(-1).unsqueeze(-1))
+
+    if return_attn_mask:
+        return embeddings, mask
+
+    return embeddings
